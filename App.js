d910826325594bb1de373428e817b9898dff6b1c
pnellesen <pnellesen@hotmail.com>
2018-08-15 22:28:05 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import AddEntry from './components/AddEntry'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AddEntry/>
+
+      </View>
+    );
+  }
+}
+
---
+++ components/AddEntry.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import UdaciSlider  from './UdaciSlider'
+import UdaciSteppers  from './UdaciSteppers'
+import DateHeader from './DateHeader'
+
+function SubmitButton ({ onPress }) {
+    return (
+        <TouchableOpacity
+            onPress={onPress}>
+            <Text>Submit</Text>
+        </TouchableOpacity>
+
+    )
+}
+
+
+export default class AddEntry extends Component {
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        eat: 0,
+        sleep: 0
+    }
+
+    increment = (metric) => {
+        const {max, step} = getMetricMetaInfo(metric)
+        this.setState((state) => {
+            const count = state[metric] + step
+            return {
+                ...state,
+                [metric]: count > max ? max : count
+            }
+        })
+    }
+
+    decrement = (metric) => {
+        const { step } = getMetricMetaInfo(metric)
+        this.setState((state) => {
+            const count = state[metric] - step
+            return {
+                ...state,
+                [metric]: count < 0 ? 0 : count
+            }
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }))
+    }
+
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state;
+        // Will update other things here as we progress
+
+        this.setState(() => ({
+            run: 0,
+            bike: 0,
+            swim: 0,
+            eat: 0,
+            sleep: 0
+        }))
+    }
+
+    render() {
+        const metaInfo = getMetricMetaInfo();
+        return (
+            <View>
+                <DateHeader date={(new Date()).toLocaleDateString()}/>
+                {
+                    Object.keys(metaInfo).map((key) => {
+                        const {getIcon, type, ...rest} = metaInfo[key]
+                        const value = this.state[key]
+                        return (
+                            <View key={key}>
+                                {getIcon()}
+                                {type === 'slider'
+                                    ? <UdaciSlider
+                                        value={value}
+                                        onChange = {(value) => this.slide(key, value)}
+                                        {...rest}/>
+                                    : <UdaciSteppers
+                                        value={value}
+                                        onIncrement={() => this.increment(key)}
+                                        onDecrement={() => this.decrement(key)}
+                                        {...rest}/>
+                                }
+                            </View>
+                        )
+                    })
+                }
+                <SubmitButton onPress={this.submit}/>
+            </View>
+        )
+    }
+}
