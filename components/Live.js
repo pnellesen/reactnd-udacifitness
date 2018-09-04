e9d9945b7970f2f219bc291ba4facb5734351812
pnellesen <pnellesen@hotmail.com>
2018-09-04 17:47:42 -0500

    Added location functions to Live component

--- components/Live.js
+++ components/Live.js
@@ -1,12 +1,73 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { Foundation } from '@expo/vector-icons'
+import { purple, white } from '../utils/colors'
+import { Location, Permissions } from 'expo'
+import { calculateDirection } from '../utils/helpers'
+
 
 class Live extends Component {
     state = {
         coords: null,
-        status: null,
+        status: 'granted',
         direction: ''
     }
+    componentDidMount() {
+        Permissions.getAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted') {
+                    return this.setLocation()
+                } else {
+                    this.setState({status: status})
+                }
+
+            })
+            .catch((error) => {
+                console.warn('Error getting location Permission: ', error)
+                this.setState({status: 'undetermined'})
+            })
+    }
+
+    askPermission = () => {
+        Permissions.askAsync(Permissions.LOCATION)
+            .then(({status}) => {
+                if (status === 'granted') {
+                    return this.setLocation()
+                } else {
+                    this.setState({status: status})
+                }
+            })
+            .catch((error) => {
+                console.warn('Error asking user for location Permission: ', error)
+                this.setState({status: 'undetermined'})
+            })
+    }
+
+    setLocation = () => {
+        Location.watchPositionAsync({
+            enableHighAccuracy: true,
+            timeInterval: 1,
+            distanceInterval: 1
+        }, ({ coords }) => {
+            const newDirection = calculateDirection(coords.heading)
+            const { direction } = this.state
+            /*
+            if (newDirection !== direction) {
+                this.setState({direction: newDirection})
+            }
+            if (coords !== this.state.coords) {
+                this.setState({coords: coords})
+            }
+            if (this.state.status !== 'granted') this.setState({status: 'granted'})
+            */
+           this.setState({
+               coords,
+               status: 'granted',
+               direction: newDirection
+           })
+        })
+    }
+
     render() {
         const { status, coords, direction } = this.state
 
@@ -14,27 +75,114 @@ class Live extends Component {
 
         if (status === 'denied') {// permission to access apis was not given by device user
             return (
-                <View>
-                    <Text>Permission Denied</Text>
+                <View style={styles.center}>
+                    <Foundation name='alert' size={50}/>
+                    <Text>Location services for this app have been denied. You can change this by enabling them in your device settings</Text>
+                    <TouchableOpacity onPress={this.askPermission}  style={styles.button}>
+                        <Text style={styles.buttonText}>Settings</Text>
+                    </TouchableOpacity>
                 </View>
             )
         }
 
         if (status === 'undetermined') {// tried to get permission but no response/weird response/something not expected
             return (
-                <View>
-                    <Text>Permission Undetermined</Text>
+                <View style={styles.center}>
+                    <Foundation name='alert' size={50}/>
+                    <Text>You need to enable location services to use the Live features of this app</Text>
+                    <TouchableOpacity onPress={this.askPermission}  style={styles.button}>
+                        <Text style={styles.buttonText}>Enable</Text>
+                    </TouchableOpacity>
                 </View>
             )
         }
 
         return (// Access has been granted
-            <View>
-                <Text>Live </Text>
-                <Text>{JSON.stringify(this.state)}</Text>
+            <View style={styles.container}>
+                <View style={styles.directionContainer}>
+                    <Text style={styles.header}>You're heading</Text>
+                    <Text style={styles.direction}>{direction}</Text>
+                </View>
+                <View style={styles.metricContainer}>
+                    <View style={styles.metric}>
+                        <Text style={[styles.header, {color: white}]}>
+                            Altitude:
+                        </Text>
+                        <Text style={[styles.subHeader, {color: white}]}>
+                            {coords && Math.round(coords.altitude)} Meters
+                        </Text>
+                    </View>
+
+                    <View style={styles.metric}>
+                        <Text style={[styles.header, {color: white}]}>
+                            Speed:
+                        </Text>
+                        <Text style={[styles.subHeader, {color: white}]}>
+                            {coords && coords.speed.toFixed(1)} M/S
+                        </Text>
+                    </View>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'space-between'
+    },
+    center: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 30,
+      marginRight: 30,
+    },
+    button: {
+      padding: 10,
+      backgroundColor: purple,
+      alignSelf: 'center',
+      borderRadius: 5,
+      margin: 20,
+    },
+    buttonText :{
+      color: white,
+      fontSize: 20,
+    },
+    directionContainer: {
+        flex: 1,
+        justifyContent: 'center',
+      },
+      header: {
+        fontSize: 35,
+        textAlign: 'center',
+      },
+      direction: {
+        color: purple,
+        fontSize: 120,
+        textAlign: 'center',
+      },
+      metricContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: purple,
+      },
+      metric: {
+        flex: 1,
+        paddingTop: 15,
+        paddingBottom: 15,
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        marginTop: 20,
+        marginBottom: 20,
+        marginLeft: 10,
+        marginRight: 10,
+      },
+      subHeader: {
+        fontSize: 25,
+        textAlign: 'center',
+        marginTop: 5,
+      },
+  })
 
 export default Live
